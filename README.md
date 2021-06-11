f339e6f8baf6b597bb19c6de3fb1028a029433b9
Ricardo Hui <rcdhui@gmail.com>
2021-06-11 09:39:47 +0800

    add acstream endpoint for endless fetching

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # java-spring-reactive
 demo how to use reactive Spring with Java Stream API
 
+To get a stream of aircraft position, after installing the project and start up the Spring application. Go to 
+http://localhost:8080/acstream
+and it will fetch new data endlessly. 
 
 planefinder at localhost:7634/aircraft is the server emiting aircraft events.
 The server indeed call an external service  http://192.168.1.193/ajax/aircraft to retrieve aircraft positions.
-aircraft-position at localhost:8080/ aircraft is the client make use of the server.
+aircraft-position at localhost:8080/aircraft is the client make use of the server.
 
 based on this book
 https://learning.oreilly.com/library/view/spring-boot-up/9781492076971/ch08.html
--- aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/PositionController.java
+++ aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/PositionController.java
@@ -1,7 +1,5 @@
 package com.thehecklers.aircraftpositions;
 
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.stereotype.Controller;
@@ -13,38 +11,38 @@ import reactor.core.publisher.Flux;
 
 @Controller
 public class PositionController {
-    @NonNull
     private final AircraftRepository repository;
     private final RSocketRequester requester;
     private WebClient client =
             WebClient.create("http://localhost:7634/aircraft");
 
-    public PositionController(AircraftRepository repository, RSocketRequester.Builder builder){
+    public PositionController(AircraftRepository repository,
+                              RSocketRequester.Builder builder) {
         this.repository = repository;
         this.requester = builder.tcp("localhost", 7635);
     }
+
+    // HTTP endpoint, HTTP requester (previously created)
     @GetMapping("/aircraft")
     public String getCurrentAircraftPositions(Model model) {
-//        repository.deleteAll();
-//
-//        client.get()
-//                .retrieve()
-//                .bodyToFlux(Aircraft.class)
-//                .filter(plane -> !plane.getReg().isEmpty())
-//                .toStream()
-//                .forEach(repository::save);
-
-        Flux<Aircraft> aircraftFlux =
-                repository.deleteAll().thenMany(client.get().retrieve().bodyToFlux(Aircraft.class).filter(
-                        plane -> !plane.getReg().isEmpty()).flatMap(repository::save));
+        Flux<Aircraft> aircraftFlux = repository.deleteAll()
+                .thenMany(client.get()
+                        .retrieve()
+                        .bodyToFlux(Aircraft.class)
+                        .filter(plane -> !plane.getReg().isEmpty())
+                        .flatMap(repository::save));
 
         model.addAttribute("currentPositions", aircraftFlux);
         return "positions";
     }
 
-        @ResponseBody
-        @GetMapping(value = "acstream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<Aircraft> getCurrentACPositionStream(){
-            return requester.route("acstream").data("Requestering aircraft positions").retrieveFlux(Aircraft.class);
-        }
+    // HTTP endpoint, RSocket client endpoint
+    @ResponseBody
+    @GetMapping(value = "/acstream",
+            produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Aircraft> getCurrentACPositionsStream() {
+        return requester.route("acstream")
+                .data("Requesting aircraft positions")
+                .retrieveFlux(Aircraft.class);
+    }
 }
